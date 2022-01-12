db47a6d2f1ece9eedb3199ef2c1c618892f134d7
Kozzztia <kozzztia@ukr.net>
2022-01-13 01:40:12 +0200

    arrowFunction start

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+const push = document.querySelector('.push');
+const menu = document.querySelector('.menu');
+const input1 = document.querySelector('.input1');
+const input2 = document.querySelector('.input2');
+const input3 = document.querySelector('.input3');
+// const select = document.querySelector('.select');
+const title = document.querySelector('.title');
+const text = document.querySelectorAll('.text');
+
