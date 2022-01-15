8483de2a62a48747382d85398c673cc7653a6f8b
Kozzztia <kozzztia@ukr.net>
2022-01-15 21:06:04 +0200

    join-split

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,4 +6,35 @@ const input3 = document.querySelector('.input3');
 // const select = document.querySelector('.select');
 const title = document.querySelector('.title');
 const text = document.querySelectorAll('.text');
+[text[0], text[1], text[2], text[3]]
+
+// -------------------------tasks
+const ar = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+]
+const txt = 'string war array'
+// --------------------------------test
+function f1(array, myFunk, out) {
+    out.innerHTML = myFunk(array)
+}
+function joinArray(array) {
+    return array.join('_')
+}
+function joinArrayStar(array) {
+    return array.join('*')
+}
+function splitArray(array) {
+    return array.split('')
+}
+function splitArraySpace(array) {
+    return array.split(' ')
+}
+push.onclick = () => {
+    f1(ar, joinArray, text[0])
+    f1(ar, joinArrayStar, text[2])
+    f1(txt, splitArray, text[1])
+    f1(txt, splitArraySpace, text[3])
+}
+
+
 
